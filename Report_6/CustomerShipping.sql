f4d99de8b623788b3b814fa4a65c5c46f3ee9173
nishaverma269 <vermanisha269@gmail.com>
2019-02-14 13:58:34 -0500

    Updated Query

--- Report_6/CustomerShipping.sql
+++ Report_6/CustomerShipping.sql
@@ -2,69 +2,49 @@
 ---------- Created by Nisha Verma on 02/06/2019 -------------
 -------------------------------------------------------------
 
-Select Q2.Customer,
-       Q2.Address,
-       Q2.Part_No,
-       Q2.Part_Name,
-       Q2.Unit_Price,
-       Sum(Q2.Shipped_Quantity) as Shipped_Quantity,
-       Sum(Q2.Shipped_Amount) as Shipped_Amount
-From
-(Select Q1.Customer,
-       Q1.Address,
-       Q1.Part_No,
-       Q1.Part_Name,
-       Q1.Ship_Date,
-       Q1.Unit_Price,
-       Q1.Quantity as Shipped_Quantity,
-       Sum(Q1.Shipped_Amount) as Shipped_Amount
-From
-(Select A.Customer_Name as Customer,
-       A.Customer_Address_Code, 
-       A.Address as Address,
-       A.Part_No as Part_No,
-       A.Customer_Part_Description as Part_Name,
-       Format(A.Ship_Date,'yyyy/MM/dd') as Ship_Date, 
-       B.Price as Unit_Price,
-       B.Quantity as Quantity,
-       (B.Price * A.Quantity) as Shipped_Amount
-From Part_v_Container_Label as A, Sales_v_Shipper_Line as B
-Where A.Shipper_Line_Key = B.Shipper_Line_Key
-  AND A.Ship_Date Between @StartDate And @EndDate-1
-  AND A.Customer_Name LIKE @Customer + '%'
-  AND A.Address LIKE @Customer_Address + '%'
-  AND A.Part_No LIKE @Part_No + '%') as Q1
-Group By Q1.Ship_Date, Q1.Customer, Q1.Address, Q1.Part_No, Q1.Part_Name, Q1.Unit_Price, Q1.Quantity) as Q2
-Group By Q2.Unit_Price, Q2.Customer, Q2.Address, Q2.Part_No, Q2.Part_Name
+Select D.Name as Customer,
+       E.Customer_Address_Code as Address_Code,
+       F.Part_Group,
+       C.Part_No as Part_No, C.Name as Part_Name,
+       B.Price as Unit_Price, 
+       Sum(B.Quantity) as Shipped_Quantity,
+       Sum((B.Price * B.Quantity)) as Shipped_Amount
+From Sales_v_Shipper as A, Sales_v_Shipper_Line as B, Part_v_Part as C, Common_v_Customer as D, Common_v_customer_address as E, Part_v_Part_Group as F
+Where A.Shipper_Key = B.Shipper_Key
+  AND B.Part_Key = C.Part_Key
+  AND A.Customer_No = D.Customer_No
+  AND A.Customer_Address_No = E.Customer_Address_No
+  AND C.Part_Group_Key = F.Part_Group_Key
+  AND A.Ship_Date Between @StartDate And @EndDate
+  AND D.Name LIKE @Customer + '%'
+  AND E.Customer_Address_Code LIKE @Customer_Address + '%'
+  AND C.Part_No LIKE @Part_No + '%'
+  AND F.Part_Group LIKE @Part_Group + '%'
+Group By B.Price, C.Part_No, C.Name, D.Name, E.Customer_Address_Code, F.Part_Group
+Order By D.Name
 
 
 ------------ Displays Customer Shipping Details -------------
 ---------- Created by Nisha Verma on 02/06/2019 -------------
 -------------------------------------------------------------
-
-Select Q1.Customer,
-       Q1.Address,
-       Q1.Part_No,
-       Q1.Part_Name,
-       Q1.Ship_Date,
-       Q1.Unit_Price,
-       Q1.Quantity as Shipped_Quantity,
-       Sum(Q1.Shipped_Amount) as Shipped_Amount
-From
-(Select A.Customer_Name as Customer,
-       A.Customer_Address_Code, 
-       A.Address as Address,
-       A.Part_No as Part_No,
-       A.Customer_Part_Description as Part_Name,
-       Format(A.Ship_Date,'yyyy/MM/dd') as Ship_Date, 
-       B.Price as Unit_Price,
-       B.Quantity as Quantity,
-       (B.Price * A.Quantity) as Shipped_Amount
-From Part_v_Container_Label as A, Sales_v_Shipper_Line as B
-Where A.Shipper_Line_Key = B.Shipper_Line_Key
-  AND A.Ship_Date Between @StartDate And @EndDate-1
-  AND A.Customer_Name LIKE @Customer + '%'
-  AND A.Address LIKE @Customer_Address + '%'
-  AND A.Part_No LIKE @Part_No + '%') as Q1
-Group By Q1.Ship_Date, Q1.Customer, Q1.Address, Q1.Part_No, Q1.Part_Name, Q1.Unit_Price, Q1.Quantity
-Order By Q1.Ship_Date
+Select D.Name as Customer,
+       E.Customer_Address_Code as Address_Code,
+       F.Part_Group,
+       C.Part_No as Part_No, C.Name as Part_Name,
+       B.Price as Unit_Price, 
+       Format(A.Ship_Date, 'yyyy/MM/dd') as Shipped_Date,
+       B.Quantity as Shipped_Quantity,
+       (B.Price * B.Quantity) as Shipped_Amount
+From Sales_v_Shipper as A, Sales_v_Shipper_Line as B, Part_v_Part as C, Common_v_Customer as D, Common_v_customer_address as E, Part_v_Part_Group as F
+Where A.Shipper_Key = B.Shipper_Key
+  AND B.Part_Key = C.Part_Key
+  AND A.Customer_No = D.Customer_No
+  AND A.Customer_Address_No = E.Customer_Address_No
+  AND C.Part_Group_Key = F.Part_Group_Key
+  AND A.Ship_Date Between @StartDate And @EndDate
+  AND D.Name LIKE @Customer + '%'
+  AND E.Customer_Address_Code LIKE @Customer_Address + '%'
+  AND C.Part_No LIKE @Part_No + '%'
+  AND B.Price LIKE @Unit_Price + '%'
+Group By A.Ship_Date, B.Price, B.Quantity, C.Part_No, C.Name, D.Name, E.Customer_Address_Code, F.Part_Group
+Order BY A.Ship_Date
--- Report_6/README.md
+++ Report_6/README.md
@@ -1,4 +1,4 @@
 This report is written to display the Customer Shipping Summary. 
-A user can filter on Date(*required field), Customer, Customer Address and Part No. to quickly retreive the required information.
-This report opens a dialog window once a user clicks on the Part No link. A dialog window will display all the details including shipment date of Customer Shipping.
+A user can filter on Date(*required field), Customer, Customer Address, Part Group and Part No. to quickly retreive the required information.
+This report groups by customer which shows the total shipped quantity and shipped amount. Also, it opens a dialog window once a user clicks on the Part No link. A dialog window will display all the details including shipment date of Customer Shipping.
 
